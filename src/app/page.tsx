da12d68ee01fe4b9058bcc7b72fbe1820fc6355c
Upanshu Parekh <54222922+blc5-33@users.noreply.github.com>
2025-01-04 21:29:13 -0600

    figured out angle stuff for hero header box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <>
       <div className="h-screen flex flex-col overflow-hidden">
         <Navbar />
-        <div className="grow bg-gradient-to-b from-neutral-800 to-neutral-500 flex flex-col-reverse">
-          <p>test</p>
+        <div className="grow bg-gradient-to-b from-neutral-800 to-neutral-500 relative">
+            <div className="w-[200%] min-w-44 min-h-64 h-[43%] bg-black opacity-40 text-white absolute inset-x-0 bottom-0 origin-bottom-right -rotate-[calc((480px/100vw)*6deg)]"></div>
         </div>
       </div>
       <div className="h-10 bg-slate-100">
