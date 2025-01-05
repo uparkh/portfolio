82a6f0b82bc0f361af69f153410f555b48c3c792
Upanshu Parekh <54222922+blc5-33@users.noreply.github.com>
2025-01-05 00:50:05 -0600

    hero header done

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -2,23 +2,23 @@ import Image from "next/image";
 import { JSX } from "react";
 
 export default function Navbar(): JSX.Element {
-    return (
-        <div className="flex justify-between items-center py-4 px-6 border-b-2 border-neutral-300 bg-gray-950">
-            <Image
-                src="/upanshu-parekh-logo.svg"
-                alt="Upanshu Parekh's Logo"
-                width={32}
-                height={32}
-                priority
-            />
-            <Image
-                src="/font-awesome/bars-solid.svg"
-                alt="Menu Bars"
-                width={32}
-                height={32}
-                className="invert"
-                priority
-            />
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-between border-b-2 border-neutral-300 bg-gray-950 px-6 py-4">
+      <Image
+        src="/upanshu-parekh-logo.svg"
+        alt="Upanshu Parekh's Logo"
+        width={32}
+        height={32}
+        priority
+      />
+      <Image
+        src="/font-awesome/bars-solid.svg"
+        alt="Menu Bars"
+        width={32}
+        height={32}
+        className="invert"
+        priority
+      />
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,41 @@
 import Navbar from "./_components/navbar";
+import Image from "next/image";
+import { JSX } from "react";
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
     <>
       <div className="flex h-screen flex-col overflow-hidden">
         <Navbar />
         <div className="relative grow bg-gradient-to-b from-neutral-800 to-neutral-500">
-          <div className="absolute inset-x-0 bottom-0 h-[43%] min-h-64 w-[200%] min-w-44 origin-bottom-right -rotate-[calc((480px/100vw)*6deg)] bg-black text-white opacity-40"></div>
-          <div className=""></div>
+          <div className="absolute inset-x-0 bottom-0 h-2/5 min-h-72 w-[200%] min-w-44 origin-bottom-right -rotate-[calc((480px/100vw)*7deg)] bg-black text-white opacity-40"></div>
+          <div className="absolute inset-x-0 bottom-0 grid h-1/3 min-h-56 grid-cols-6 gap-3 p-6">
+            <div className="col-span-5 flex flex-col justify-evenly p-4 pt-14 font-raleway font-bold text-white">
+              <p className="text-xl">Hi, I am</p>
+              <h1 className="text-4xl">Upanshu Parekh</h1>
+              <h4 className="text-lg">Aspiring Software Engineer</h4>
+            </div>
+            <div className="flex flex-col items-center justify-around">
+              <Image
+                src="/at-sign.svg"
+                alt="At sign for Upanshu's email"
+                width={32}
+                height={32}
+              />
+              <Image
+                src="/github-logo.svg"
+                alt="Github logo for Upanshu's Github account"
+                width={32}
+                height={32}
+              />
+              <Image
+                src="/linkedin-circle.svg"
+                alt="Linkedin logo for Upanshu's Linkedin account"
+                width={32}
+                height={32}
+              />
+            </div>
+          </div>
         </div>
       </div>
       <div className="h-10 bg-slate-100">test 2</div>
