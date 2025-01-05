e5660d858d1b943b4e473d1b3bf7518254bbd39c
Upanshu Parekh <54222922+blc5-33@users.noreply.github.com>
2025-01-05 02:13:58 -0600

    added my portrait and got responsiveness right

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,19 @@ import { JSX } from "react";
 export default function Home(): JSX.Element {
   return (
     <>
-      <div className="flex h-screen flex-col overflow-hidden">
+      <div className="flex h-screen max-h-[900px] min-w-[441px] flex-col overflow-hidden">
         <Navbar />
-        <div className="relative grow bg-gradient-to-b from-neutral-800 to-neutral-500">
-          <div className="absolute inset-x-0 bottom-0 h-2/5 min-h-72 w-[200%] min-w-44 origin-bottom-right -rotate-[calc((480px/100vw)*7deg)] bg-black text-white opacity-40"></div>
-          <div className="absolute inset-x-0 bottom-0 grid h-1/3 min-h-56 grid-cols-6 gap-3 p-6">
+        <div className="relative -z-20 grow bg-gradient-to-b from-neutral-800 to-neutral-500">
+          <div className="center absolute bottom-0 left-0 right-0 top-1 -z-10 mx-auto max-w-[30rem] origin-top translate-x-4 -scale-x-125 scale-y-125">
+            <Image
+              src="/boss.png"
+              alt="Upanshu's portrait"
+              width={2000}
+              height={2992}
+            />
+          </div>
+          <div className="absolute inset-x-0 bottom-0 z-0 h-2/5 min-h-72 w-[200%] min-w-44 origin-bottom-right -rotate-[calc((480px/100vw)*7deg)] bg-black text-white opacity-40"></div>
+          <div className="absolute inset-x-0 bottom-0 z-10 grid h-1/3 min-h-56 grid-cols-6 gap-3 p-6">
             <div className="col-span-5 flex flex-col justify-evenly p-4 pt-14 font-raleway font-bold text-white">
               <p className="text-xl">Hi, I am</p>
               <h1 className="text-4xl">Upanshu Parekh</h1>
