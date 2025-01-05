78860d8c0de168264532d6b508a3eae92b4f301c
Upanshu Parekh <54222922+blc5-33@users.noreply.github.com>
2025-01-04 21:58:51 -0600

    initialized prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,14 @@ import Navbar from "./_components/navbar";
 export default function Home() {
   return (
     <>
-      <div className="h-screen flex flex-col overflow-hidden">
+      <div className="flex h-screen flex-col overflow-hidden">
         <Navbar />
-        <div className="grow bg-gradient-to-b from-neutral-800 to-neutral-500 relative">
-            <div className="w-[200%] min-w-44 min-h-64 h-[43%] bg-black opacity-40 text-white absolute inset-x-0 bottom-0 origin-bottom-right -rotate-[calc((480px/100vw)*6deg)]"></div>
+        <div className="relative grow bg-gradient-to-b from-neutral-800 to-neutral-500">
+          <div className="absolute inset-x-0 bottom-0 h-[43%] min-h-64 w-[200%] min-w-44 origin-bottom-right -rotate-[calc((480px/100vw)*6deg)] bg-black text-white opacity-40"></div>
+          <div className=""></div>
         </div>
       </div>
-      <div className="h-10 bg-slate-100">
-        test 2
-      </div>
-
+      <div className="h-10 bg-slate-100">test 2</div>
     </>
   );
 }
