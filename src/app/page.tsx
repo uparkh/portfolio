04d68771a8135415cab0d8342f730b25cf11407d
Upanshu Parekh <54222922+blc5-33@users.noreply.github.com>
2025-01-06 01:15:15 -0600

    refactor to not need to specify width and height for Images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,16 @@ import Navbar from "./_components/navbar";
 import ResumeSectionH1 from "./_components/resume-section-h1";
 import ResumeSectionText from "./_components/resume-section-text";
 import ResumeExperienceH2 from "./_components/resume-experience-h2";
+
 import Image from "next/image";
+import portrait from "/public/boss.png";
+import atSign from "/public/at-sign.svg";
+import githubLogo from "/public/github-logo.svg";
+import linkedinLogo from "/public/linkedin-circle.svg";
+import uiucCsLogo from "/public/uiuc-cs-logo.png";
+import codehsLogo from "/public/codehs-logo.png";
+import iemLogo from "/public/iem-logo.png";
+
 import { JSX } from "react";
 
 export default function Home(): JSX.Element {
@@ -12,12 +21,7 @@ export default function Home(): JSX.Element {
         <Navbar />
         <div className="relative z-0 grow bg-gradient-to-b from-neutral-800 to-neutral-500">
           <div className="absolute bottom-0 left-0 right-0 top-1 -z-10 mx-auto max-w-[30rem] origin-top translate-x-4 -scale-x-125 scale-y-125">
-            <Image
-              src="/boss.png"
-              alt="Upanshu's portrait"
-              width={2000}
-              height={2992}
-            />
+            <Image src={portrait} alt="Upanshu's portrait" priority />
           </div>
           <div className="absolute inset-x-0 bottom-0 z-0 h-2/5 min-h-72 w-[200%] min-w-44 origin-bottom-right -rotate-[calc((480px/100vw)*7deg)] bg-black text-white opacity-70"></div>
           <div className="absolute inset-x-0 bottom-0 z-10 grid h-1/3 min-h-56 grid-cols-6 gap-3 p-6">
@@ -32,10 +36,10 @@ export default function Home(): JSX.Element {
                 className="hover:brightness-75"
               >
                 <Image
-                  src="/at-sign.svg"
+                  src={atSign}
                   alt="At sign to Upanshu's email"
-                  width={32}
-                  height={32}
+                  className="w-8"
+                  priority
                 />
               </a>
               <a
@@ -45,10 +49,10 @@ export default function Home(): JSX.Element {
                 className="hover:brightness-75"
               >
                 <Image
-                  src="/github-logo.svg"
+                  src={githubLogo}
                   alt="Github logo to Upanshu's Github account"
-                  width={32}
-                  height={32}
+                  className="w-8"
+                  priority
                 />
               </a>
               <a
@@ -58,10 +62,10 @@ export default function Home(): JSX.Element {
                 className="hover:brightness-75"
               >
                 <Image
-                  src="/linkedin-circle.svg"
+                  src={linkedinLogo}
                   alt="Linkedin logo to Upanshu's Linkedin account"
-                  width={32}
-                  height={32}
+                  className="w-8"
+                  priority
                 />
               </a>
             </div>
@@ -71,11 +75,9 @@ export default function Home(): JSX.Element {
       <div className="bg-main-slate flex flex-col items-center gap-5 p-14 text-white">
         <ResumeSectionH1 className="border-white">ABOUT ME</ResumeSectionH1>
         <Image
-          src="/uiuc-cs-logo.png"
-          width={200}
-          height={200}
+          src={uiucCsLogo}
           alt="UIUC CS Logo"
-          className="rounded-2xl"
+          className="w-48 rounded-2xl"
         ></Image>
         <ResumeSectionText>
           Hi! 👋 I’m Upanshu (oo-pahn-shoe), I’m a 4th year B.S. student at the
@@ -102,14 +104,12 @@ export default function Home(): JSX.Element {
         </ResumeSectionText>
       </div>
       <div className="bg-main-ivory p-14 text-black">
-        <div className="flex flex-col items-center gap-5 border-b-2 border-black pb-5">
+        <div className="flex flex-col items-center gap-5 border-b-2 border-black pb-10">
           <ResumeSectionH1 className="border-black">EXPERIENCE</ResumeSectionH1>
           <Image
-            src="/codehs-logo.png"
-            width={200}
-            height={200}
-            alt="UIUC CS Logo"
-            className="rounded-lg"
+            src={codehsLogo}
+            alt="CodeHS Logo"
+            className="w-52 rounded-lg"
           ></Image>
           <div>
             <ResumeExperienceH2>CodeHS</ResumeExperienceH2>
@@ -121,6 +121,26 @@ export default function Home(): JSX.Element {
               on a word embedding database, as well as a realtime quiz
               leaderboard prototype for Questions of the Day (similar to
               Kahoot!).
+            </ResumeSectionText>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-5 pt-10">
+          <ResumeSectionH1 className="border-black">
+            EXTRACURRICULARS
+          </ResumeSectionH1>
+          <div className="relative h-56 w-96">
+            <Image src={iemLogo} alt="Illini Electric Motorsports Logo" />
+          </div>
+          <div>
+            <ResumeExperienceH2>Formula SAE</ResumeExperienceH2>
+            <ResumeSectionText>
+              Illini Electric Motorsports is about developing a racecar from
+              scratch every year to compete internationally against other
+              universities’ formula-spec cars. {"\n\n"} I was a member of the
+              Data Acquisition and Quantitative Analysis team from Jan-Apr 2024,
+              and saved the team $700+ by developing an in-house Python file
+              format converter for the MoTeC i2 telemetry software (branching
+              from existing open-source tools).
             </ResumeSectionText>
           </div>
         </div>
