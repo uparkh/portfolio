7639c431df8ed875e081ad892faa32037e7cf796
Upanshu Parekh <54222922+blc5-33@users.noreply.github.com>
2025-01-10 16:55:19 -0600

    basic animation done!

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,33 +1,53 @@
+"use client";
+
 import Image from "next/image";
 import upanshuParekhLogo from "/public/upanshu-parekh-logo.svg";
 import barsSolid from "/public/font-awesome/bars-solid.svg";
 import arrowFromBox from "/public/font-awesome/arrow-up-right-from-square-solid.svg";
-import { JSX } from "react";
+import closeXSolid from "/public/font-awesome/x-solid.svg";
+import { JSX, useState } from "react";
 
 export default function Navbar(): JSX.Element {
+  const [isMenuOpen, setDropdownActive] = useState<boolean>(false);
+
+  function handleDropdown(): void {
+    setDropdownActive(!isMenuOpen);
+  }
+
   return (
-    <nav className="absolute z-40 w-screen overflow-visible">
+    <nav
+      className={`${isMenuOpen ? "fixed" : "absolute"} z-40 w-screen overflow-visible`}
+    >
       <div className="relative z-20 flex items-center justify-between border-b-2 border-neutral-300 bg-gray-950 px-6 py-4">
         <Image
           src={upanshuParekhLogo}
           alt="Upanshu Parekh's Logo"
-          sizes="9vw"
-          className="w-[9vw] max-w-9"
+          sizes="2.25rem"
+          className="max-h-9 w-[9vw]"
           priority
         />
-        <button className="rounded-m flex items-center justify-end gap-3">
-          <p className="font-raleway text-lg font-bold text-white">MENU</p>
+        <button
+          className="rounded-m flex items-center justify-end gap-3"
+          onClick={handleDropdown}
+        >
+          <p className="font-raleway text-lg font-bold text-white">
+            {isMenuOpen ? "CLOSE" : "MENU"}
+          </p>
           <Image
-            src={barsSolid}
-            alt="Menu Bars"
-            sizes="9vw"
-            className="w-[9vw] max-w-9 invert"
+            src={isMenuOpen ? closeXSolid : barsSolid}
+            alt={isMenuOpen ? "Close Menu" : "Open Menu"}
+            sizes="2.25rem"
+            className="max-h-9 w-[9vw] invert"
             priority
           />
         </button>
       </div>
-      <div className="absolute -z-10 h-screen w-screen bg-black bg-opacity-60"></div>
-      <ul className="dropdown-active z-0 flex flex-col bg-gray-950 bg-opacity-85 text-center font-montserrat text-lg font-bold tracking-widest text-white">
+      <div
+        className={`${isMenuOpen ? "fade-in" : "fade-out"} absolute -z-10 h-screen w-screen bg-black bg-opacity-60`}
+      ></div>
+      <ul
+        className={`${isMenuOpen ? "dropdown-active" : "dropdown-inactive"} z-0 flex flex-col bg-gray-950 bg-opacity-75 text-center font-montserrat text-lg font-bold tracking-widest text-white`}
+      >
         <a href="#about-me">
           <li className="border-1 border-neutral-300 px-6 py-4">ABOUT ME</li>
         </a>
@@ -38,7 +58,7 @@ export default function Navbar(): JSX.Element {
           <li className="border-1 border-neutral-300 px-6 py-4">PORTFOLIO</li>
         </a>
         <a href="#">
-          <li className="border-1 flex items-center justify-center gap-3 border-neutral-300 bg-neutral-600 bg-opacity-40 px-6 py-4 text-white">
+          <li className="border-1 flex items-center justify-center gap-3 border-neutral-300 bg-neutral-700 bg-opacity-75 px-6 py-4 text-white">
             RESUME
             <Image
               src={arrowFromBox}
@@ -50,7 +70,7 @@ export default function Navbar(): JSX.Element {
           </li>
         </a>
         <a href="#contact">
-          <li className="border-1 border-neutral-300 bg-neutral-300 px-6 py-4 text-black">
+          <li className="border-1 border-neutral-300 bg-neutral-300 bg-opacity-75 px-6 py-4 text-black">
             CONTACT
           </li>
         </a>
